832cb703f55dec1194d7cfca663e73ae9846398e
progre <djyayutto@gmail.com>
2017-04-09 14:35:00 +0900

    Fix init

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-try { require('source-map-support').install(); } catch (e) { /* NOP */ }
-import module from './module';
-
-async function main() {
-  module() ;
-}
-
-main().catch(e => console.error(e.stack || e));
