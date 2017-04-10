061ce442fa11c4d9186eda9afa18e256ec578700
progre <djyayutto@gmail.com>
2017-04-10 22:03:54 +0900

    Fix travis

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,1 +1,73 @@
-../server/webpack.config.js
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const failPlugin = require("webpack-fail-plugin");
+const uglifySaveLicense = require("uglify-save-license");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+let common = {
+  devtool: isProduction
+    ? false
+    : "inline-source-map",
+  node: {
+    __filename: true,
+    __dirname: true
+  },
+  plugins: isProduction
+    ? [failPlugin]
+    : [],
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  watchOptions: {
+    ignored: /node_modules|lib/
+  }
+};
+
+function tsModule(targets) {
+  return {
+    rules: [{
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            env: {
+              development: {
+                plugins: [[
+                  "babel-plugin-espower",
+                  { "embedAst": true }
+                ]]
+              },
+              production: {
+                presets: ["babili"]
+              }
+            },
+            presets: [["env", { targets }]]
+          }
+        },
+        {
+          loader: "ts-loader",
+          options: { compilerOptions: { sourceMap: !isProduction } }
+        }
+      ]
+    }]
+  };
+}
+
+module.exports = [
+  Object.assign({},
+    common,
+    {
+      entry: {
+        index: ["./src/index.ts"],
+        "test/test": ["./src/test/test.ts"]
+      },
+      externals: /^(?!\.)/,
+      module: tsModule({ node: 6 }),
+      output: {
+        filename: "lib/[name].js",
+        libraryTarget: "commonjs2"
+      },
+      target: "node"
+    }
+  )
+];
