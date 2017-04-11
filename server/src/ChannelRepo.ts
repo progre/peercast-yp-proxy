b103a8cc17662af89ef8989874e05579b5fa593e
progre <djyayutto@gmail.com>
2017-04-11 22:21:44 +0900

    Fix

--- server/src/ChannelRepo.ts
+++ server/src/ChannelRepo.ts
@@ -1,7 +1,8 @@
 import fetch from 'node-fetch'; // tslint:disable-line:import-name
 import { Channel, parse } from 'peercast-yp-channels-parser';
 import { Subject } from 'rxjs';
-import { Directive } from '../../common/messages';
+import { deepEqualOrCreatedAtNearTime } from './common/channelutils';
+import { Directive } from './common/messages';
 
 // let TP_OSHIRASE = 'TPからのお知らせ◆お知らせ';
 // let TP_UPLOAD = 'Temporary yellow Pages◆アップロード帯域';
@@ -49,7 +50,7 @@ function getDiffList(
   return {
     deleteList: old.filter(x => now.every(y => x.id !== y.id)),
     setList: now.filter(x => old.every(
-      y => !deepEqualOrNearCreatedAt(x, y),
+      y => !deepEqualOrCreatedAtNearTime(x, y),
     )), // include updates
   };
 }
@@ -63,40 +64,4 @@ async function fetchChannels() {
     date: now,
     channels: <ReadonlyArray<Channel>>channels.concat(parse(await res.text(), now)),
   };
-}
-
-function deepEqualOrNearCreatedAt(
-  a: Channel,
-  b: Channel,
-) {
-  if (
-    a.name !== b.name ||
-    a.id !== b.id ||
-    a.ip !== b.ip ||
-    a.url !== b.url ||
-    a.genre !== b.genre ||
-    a.desc !== b.desc ||
-    a.bandwidthType !== b.bandwidthType ||
-    a.listeners !== b.listeners ||
-    a.relays !== b.relays ||
-    a.bitrate !== b.bitrate ||
-    a.type !== b.type ||
-    a.track.creator !== b.track.creator ||
-    a.track.album !== b.track.album ||
-    a.track.title !== b.track.title ||
-    a.track.url !== b.track.url ||
-    // createdAt isn't checked.
-    a.comment !== b.comment ||
-    a.direct !== b.direct
-  ) {
-    return false;
-  }
-  // difference of 2 minutes or more
-  if (
-    Math.abs(a.createdAt - b.createdAt)
-    > 2 * 60 * 1000
-  ) {
-    return false;
-  }
-  return true;
 }
