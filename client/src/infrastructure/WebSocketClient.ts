fa23859cbf4eaa71b5f80fb69c15c83e7ccd3b99
progre <djyayutto@gmail.com>
2017-05-07 10:50:16 +0900

    fix: Fix client

--- client/src/infrastructure/WebSocketClient.ts
+++ client/src/infrastructure/WebSocketClient.ts
@@ -5,16 +5,17 @@ import * as messages from '../common/messages';
 export default class WebSocketClient {
   private readonly socket: SocketIOClient.Socket;
 
-  constructor(url: string) {
-    this.socket = socketIo(url);
-  }
-
   readonly error = new Observable((subscribe) => {
     Observable.fromEvent(this.socket, 'connect_error').subscribe(subscribe);
     Observable.fromEvent(this.socket, 'error').subscribe(subscribe);
     Observable.fromEvent(this.socket, 'reconnect_error').subscribe(subscribe);
   });
-  readonly message = Observable.fromEvent<messages.Message>(this.socket, 'message');
+  readonly message: Observable<messages.Message>;
+
+  constructor(url: string) {
+    this.socket = socketIo(url);
+    this.message = Observable.fromEvent<messages.Message>(this.socket, 'message');
+  }
 
   close() { this.socket.close(); }
 }
